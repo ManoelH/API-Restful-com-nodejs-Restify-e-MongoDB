58f9b4db18f98162311e742b3c3479b480348d66
manoelh <henriqe115@gmail.com>
2019-07-16 22:29:31 -0300

    otimização de código

--- users/users.router.ts
+++ users/users.router.ts
@@ -4,35 +4,28 @@ import { User } from './users.model';
 
 
 class UserRouter extends Router{
+
+    constructor(){
+        super()
+        this.on('beforeRender', document=>{
+            document.password = undefined
+            //delete documente.password
+        })
+    }
+
     applyRouters(aplication: restify.Server){
         
         aplication.get('/users', (req, resp, next)=>{
-            User.find().then(users=>{
-                resp.json(users)
-                return next()
-            })
+            User.find().then(this.render(resp, next))
         })
 
         aplication.get('users/:id', (req, resp, next)=>{
-            User.findById(req.params.id).then(user=>{
-                if(user){
-                    resp.json(user)
-                    return next()
-                }
-                else{
-                    resp.send(404)
-                    return next()
-                }
-            })
+            User.findById(req.params.id).then(this.render(resp, next))
         })
 
         aplication.post('/users', (req, resp, next)=>{
             let user = new User(req.body);
-            user.save().then(user=>{
-                user.password = undefined //used to ocult the password after to be saved
-                resp.json(user)
-                return next()
-            })
+            user.save().then(this.render(resp, next))
         })
 
         aplication.put('/users/:id', (req, resp, next)=>{
@@ -42,22 +35,12 @@ class UserRouter extends Router{
                     return User.findById(req.params.id)
                 else
                     resp.send(404)
-            }).then(user=>{
-                resp.json(user)
-                return next()
-            })
+            }).then(this.render(resp, next))
         })
 
         aplication.patch('/users/:id', (req, resp, next)=>{
             const options = {new: true} //used to indicate that the new user will be showed in resp.json and don't the old user
-            User.findByIdAndUpdate(req.params.id, req.body, options).then(user=>{
-                if(user){
-                    resp.json(user)
-                    return next()
-                }
-                else
-                    return resp.send(404)    
-            })
+            User.findByIdAndUpdate(req.params.id, req.body, options).then(this.render(resp, next))
         })
 
         aplication.del('/users/:id', (req, resp, next)=>{
